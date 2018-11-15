4b958233519f54ea7b90417df5cc58cc3c2521cc
cg.jin <chg.jin@idiaoyan.com>
2018-11-15 10:32:32 +0800

    增加路由

---
+++ vue_test/src/components/Hi.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+<script>
+  export default{
+    name: 'hi',
+    data(){
+      return {
+        msg: 'hi chg.jin'
+      }
+    }
+  }
+</script>
+<style scoped>
+  .hello{
+    font-size: 18px;
+    text-align: center;
+  }
+</style>
--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -3,8 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
+//生产环境提示，这里设置成了false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +12,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
